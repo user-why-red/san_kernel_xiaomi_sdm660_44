cb64d089685df5dce7a24ad7ec55aa8a8a6a85f4
RooGhz720 <rooghz720@gmail.com>
2023-12-08 14:19:24 +0000

    ksu: add support for linux 4.4 sdm660
    Signed-off-by: RooGhz720 <rooghz720@gmail.com>
    
    Signed-off-by: Santhosh <san8754513543@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1560,6 +1560,10 @@ static void android_service_blacklist(const char *name)
 	}
 }
 
+// KernelSU hook
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			       void *envp, int *flags);
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1573,6 +1577,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);  // call KSU hook first
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
